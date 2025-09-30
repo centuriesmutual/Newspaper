532340bc048f07771451146d4d43127f1a3500cf
oldwestsolutions <oldwestsolutions@example.com>
2025-09-30 23:55:00 +0000

    Add Vercel Analytics SDK for website tracking

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import './globals.css'
 import { Inter } from 'next/font/google'
 import Navbar from '../components/Navbar'
 import Script from 'next/script'
+import { Analytics } from '@vercel/analytics/react'
 import { metadata } from './metadata'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -24,6 +25,7 @@ export default function RootLayout({ children }) {
       <body className="overflow-x-hidden">
         <Navbar />
         {children}
+        <Analytics />
         <Script 
           src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
           integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz"
