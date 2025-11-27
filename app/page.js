c3610d651a8f73c1c817f82819bc381ee63e5d63
Centuries Mutual <github@centuriesmutual.com>
2025-11-27 21:45:36 +0100

    Add three new network product sections: Security Cameras, Network Devices, and Smart Home Security System - Powered by Lumen Technologies and Texas Instruments

--- app/page.js
+++ app/page.js
@@ -2,6 +2,9 @@ import Hero from '../components/Hero'
 import BlockchainSection from '../components/BlockchainSection'
 import RealEstate from '../components/RealEstate'
 import PaymentTech from '../components/PaymentTech'
+import SecurityCameras from '../components/SecurityCameras'
+import NetworkDevices from '../components/NetworkDevices'
+import SmartHomeSecurity from '../components/SmartHomeSecurity'
 import Community from '../components/Community'
 import Legacy from '../components/Legacy'
 import CTA from '../components/CTA'
@@ -19,6 +22,9 @@ export default function Home() {
         <RealEstate />
         <Community />
         <PaymentTech />
+        <SecurityCameras />
+        <NetworkDevices />
+        <SmartHomeSecurity />
         <Legacy />
         <CTA />
         <Footer />
